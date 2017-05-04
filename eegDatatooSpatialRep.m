692bbbed2a28366c03873acbcb9c48feef46e7a4
Titus John <titusaj92@gmail.com>
2017-05-04 11:13:07 -0400

    Upload matlab scripts

---
+++ eegDatatooSpatialRep.m
@@ -0,0 +1,25 @@
+
+%%
+%Titus John
+%May 4,2017
+
+
+%Input 
+%Raw EEG data(18-24 channels
+%Raw EEG data signal labels
+%eeg labeled 2d projections
+
+%Output
+%Spatial representation of the EEG data
+
+
+function [spatialRep] =  eegDatatooSpatialRep (eegDataRawdata, eegSignalLabels, eeg2Dproj)
+%% Match the data withi
+
+
+%% wavelet transform of the data channels
+
+
+%% 
+
+end
