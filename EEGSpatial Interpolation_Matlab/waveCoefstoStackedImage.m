ccd94bb6dd7ffcdc6fd66258f6103c850b94e2b6
Titus John <titusaj92@gmail.com>
2017-06-30 17:37:29 -0400

    working on getting the rows and the colums sorted out to create the intial image

--- EEGSpatial Interpolation_Matlab/waveCoefstoStackedImage.m
+++ EEGSpatial Interpolation_Matlab/waveCoefstoStackedImage.m
@@ -5,18 +5,6 @@
 % Take the wave coefficents and convert it to a stacked image that is then
 %fed into the classifier
 
-% input
-% inp
-
-% Output
-
-
-
-%----------------------------------------------------
-function [] = waveCoefstoStackedImage(matchedEEGData, Fs)
-   
-
-
 % Stack the wave coefficents into a image that is 
 
 
@@ -25,6 +13,39 @@ function [] = waveCoefstoStackedImage(matchedEEGData, Fs)
 
 
 % Have to stack image together to form overall image
+
+
+% input
+% input the wave coefficents for each of the channel epochs
+
+% Output
+% stacked image 
+
+
+%----------------------------------------------------
+function [stackedImage] = waveCoefstoStackedImage(wt, period,Fs)
+   
+    %Sampling specs
+    dt = 1/Fs;
+    Norig = Fs;
+   
+    % Specify the image dimensions 
+        nRows = 3;
+        nColums = 3;
+    
+        
+      % This specifys the overall time array over which the data is sampled
+        t = 0:dt:(Norig*dt*nRows)-dt;
+
+    for i =1:nRows
+        if i ==1
+            waveCatRows = wt{i};
+        else
+            waveCatRows = cat(2, wt{i},waveCatRows);
+        end
+    end
+
+
 
 
 
