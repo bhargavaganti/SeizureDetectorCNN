db3105c3ec9021188007cc6c97d8fb29ab2b9832
Titus John <titusaj92@gmail.com>
2017-07-21 14:02:01 -0400

    working on preprocessing the data for single channel analysis

--- ImportRawData_Python/ImportEDFtoMat.py
+++ ImportRawData_Python/ImportEDFtoMat.py
@@ -2,12 +2,12 @@ import pyedflib
 import numpy as np
 import numpy, scipy.io
 
-f = pyedflib.EdfReader("/Users/titusjohn/Documents/SeizureTestData/MG111113_1-1+.edf")
+f = pyedflib.EdfReader("/Users/titusjohn/Documents/EMU Seizures/SZ 2/NKT/EEG2100/MG111114_1-1+.edf")
 n = f.signals_in_file
 signal_labels = f.getSignalLabels()
 sigbufs = np.zeros((n, f.getNSamples()[0]))
 
-for i in np.arrange(n):
+for i in np.arange(n):
     sigbufs[i, :] = f.readSignal(i)
 
 print (sigbufs.shape)
---
+++ Preprocessing_Matlab/splitMatixSaveLabeled.m
@@ -0,0 +1,24 @@
+% Titus John
+% 7/21/2017
+%%%%%%%%%%%%%%%%%%%%%%
+% input -  
+% Case Name
+%Channel Number
+%Epoch Numer
+%Class -  Sleep/Sz
+%Fs - Sampling Rate
+
+% output - 
+
+%files
+%%%%%%%%%%%%%%%%%%%%%%
+
+function splitMatixSaveLabeled(CaseName, ChannelNum, EpochNum, Class,Fs)
+
+    filename = strcat(CaseName, ChannelNum, EpochNum, Class)
+
+end
+
+    
+    
+   
