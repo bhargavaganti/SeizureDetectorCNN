e4c9d006b1c1ac1437814ce93808bd354915d290
Titus John <titusaj92@gmail.com>
2017-08-03 15:57:38 -0400

    working on stacking fft from each case into one

---
+++ Preprocessing_Matlab/findMaxFFT.m
@@ -0,0 +1,17 @@
+% Titus John
+% 7/21/2017
+%%%%%%%%%%%%%%%%%%%%%%
+% input -  
+% Case Name
+%Channel Number
+%Epoch Numer
+%Class -  Sleep/Sz
+%Fs - Sampling Rate
+
+% output - 
+
+%files
+%%%%%%%%%%%%%%%%%%%%%%
+
+function findMaxFFT %(CaseName, ChannelNum, EpochNum, Class,Fs)
+end
