8b654b2da8973737fd2e7df571236b90a994bcf1
Titus John <titusaj92@gmail.com>
2017-08-07 13:20:05 -0400

    working on 128 point average

--- Preprocessing_Matlab/findMaxFFT.m
+++ Preprocessing_Matlab/findMaxFFT.m
@@ -13,5 +13,25 @@
 %files
 %%%%%%%%%%%%%%%%%%%%%%
 
-function findMaxFFT %(CaseName, ChannelNum, EpochNum, Class,Fs)
+figure(1)
+hold on
+
+for n = 2:20
+        
+        currentChannel =  (data(1726000*(n-1):1726000*n));
+
+        freqData = detrend( currentChannel);
+         % 256 point fft
+        y_fft = fft(freqData ,128) ;
+        % plotting of spectra
+        p  = abs(y_fft.^2) ;
+        df = Fs/Npoint_fft ;
+        ff = 0:df:Fs/2-df ;
+        %save(filename,'p');
+        
+        allFFT(n,:)= log(p(1:end/2)); 
+        
+        plot (ff,log(p(1:end/2)),'b')
+        count = count+1;
+    
 end
