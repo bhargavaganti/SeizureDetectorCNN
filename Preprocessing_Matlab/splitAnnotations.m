7c1a320474d7bc5879cdf187400b688fabe92da3
Titus John <titusaj92@gmail.com>
2017-08-10 11:42:55 -0400

    worked out how to split on -

--- Preprocessing_Matlab/splitAnnotations.m
+++ Preprocessing_Matlab/splitAnnotations.m
@@ -1,3 +1,20 @@
 function splitAnnotations
-
+    files  = dir;
+    
+    for i = 3:length(dir)
+        %Load the file into space and get the annotations into form
+        currentFile = files(i).name; 
+        data = importdata(currentFile,'\n');
+        rawAnn = data{3};
+        splitAnn = strsplit(rawAnn,'-');
+        
+         %look for the annotation asleep SZ and ENDS
+        for j =1:length(splitAnn)
+            
+        end
+        
+    end
+    
+   
+    
 end
