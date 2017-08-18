d28becf53c824f92f44c3b4ad9e7681179dae23a
Titus John <titusaj92@gmail.com>
2017-08-18 15:09:30 -0400

    working on the randomization process of picking test

--- Preprocessing_Matlab/createEpochTrainTest.m
+++ Preprocessing_Matlab/createEpochTrainTest.m
@@ -14,23 +14,58 @@
 %%%%%%%%%%%%%%%%%%%%%%
 function parseEpochData
 
+files = dir;
+
     for i =5
+        
+        %Get the parts of name of the SZ files
         szFolder = strcat('szNum',num2str(i));
         lastFile = files(length(files)).name; 
         lastFileParts=strsplit(lastFile,'_')
         
-        numFilePart = lastFileParts{2};
-        szEpochsNum= str2num(numFilePart(1)) %this represents the number of sz epochs in the file
+        
         
          numFilePart = lastFileParts{1};
          szFileNum  = str2num(numFilePart(end));
         
         
-        for i =1:fileNum
-            szFileName = strcat('SZEpoch',num2str(szFileNum),'_',num2str(szEpochsNum),'.txt')
-        end
+        numFilePart = lastFileParts{2};
+        szEpochsNum= str2num(numFilePart(1)) %this represents the number of sz epochs in the file
+        
+     
+        
+         figure(1)
+         hold on
+         title('SZ FFTs')
+            for j =1:szFileNum
+                szFileName = strcat('SZEpoch',num2str(szFileNum),'_', num2str(j) ,'.txt')
+                 rawSzData = csvread(szFileName);
+                 szMaxFFT= findMaxFFT(rawSzData,'r');
+
+            end
+        
+            
+            
+         %Pick of random files based on number sz files
+          %10:1
+            
+            
+            
+        
+        figure(2)
+        hold on 
+        title('SZ FFTs')
+            for j =1:sleepFileNum
+                 sleepFileName = strcat('SZEpoch',num2str(sleepFileNum),'_', num2str(j) ,'.txt')
+                 rawSleepData = csvread(sleepFileName);
+                 sleepMaxFFT= findMaxFFT(rawSleepData,'r');
+
+            end
         
     end
+    
+    
+    
     
      
 
--- Preprocessing_Matlab/findMaxFFT.m
+++ Preprocessing_Matlab/findMaxFFT.m
@@ -13,6 +13,7 @@
 %files
 %%%%%%%%%%%%%%%%%%%%%%
 
+function    MaxFFT= findMaxFFT(rawData, linecolor)
 
 Fs = 500 ;
 duration = 0.001 ;
@@ -21,26 +22,26 @@ f  = 1000 ;
 Npoint_fft = 256 ;
 
 
-figure(1)
-title('Sleep')
-hold on
+% figure(1)
+% title('Sleep')
+% hold on
 
 
-for  i = 1:length(sleepData)/500
-        if i ==1
-            sleepEpochs{i} = sleepData(1:21,1:500);
-        else
-            sleepEpochs{i} = sleepData(1:21,500*(i-1):500*i);
-        end
-        
-end
+% for  i = 1:length(sleepData)/500
+%         if i ==1
+%             sleepEpochs{i} = sleepData(1:21,1:500);
+%         else
+%             sleepEpochs{i} = sleepData(1:21,500*(i-1):500*i);
+%         end
+%         
+% end
 
  
-for i =1:length(sleepEpochs)
-    currentSleepEpoch = sleepEpochs{i}; 
+ 
+
     
         for n = 1:21
-                currentChannel =  currentSleepEpoch(n,:);
+                currentChannel =  rawData(n,:);
 
 
                 freqData = detrend( currentChannel);
@@ -54,81 +55,30 @@ for i =1:length(sleepEpochs)
 
                 allFFT(n,:)= log(p(1:end/2)); 
 
-                plot (ff,log(p(1:end/2)),'b')
-                count = count+1;
+             %   plot (ff,log(p(1:end/2)),'b')
+               
 
         end
     
         for n =1:length(allFFT(1,:))
-            sleepMaxFFT(n) = max(allFFT(:,n)); 
+            MaxFFT(n) = max(allFFT(:,n)); 
 
         end
 
-        plot(ff, sleepMaxFFT,'r')
-        
-        allMaxFFTSleep(i,:) = sleepMaxFFT;
+
+         plot(ff, MaxFFT,linecolor)
+%         
+%         allMaxFFTsz(i,:) = szMaxFFT;
     
-end
 
 
 
+% figure(3)
+% hold on 
+% for i=1: length(allMaxFFTSleep(:,1))
+%     plot(ff,allMaxFFTsz(i,:),'r')
+%       plot(ff,allMaxFFTSleep(i,:),'b')
+% end
 
 
-
-
-
-figure(2)
-title('SZ')
-hold on
-
-for  i = 1:length(szData)/500
-        if i ==1
-            szEpochs{i} = szData(1:21,1:500);
-        else
-            szEpochs{i} = szData(1:21,500*(i-1):500*i);
-        end
-        
-end
-
- 
-for i =1:length(szEpochs)
-    currentszEpoch = szEpochs{i}; 
-    
-        for n = 1:21
-                currentChannel =  currentszEpoch(n,:);
-
-
-                freqData = detrend( currentChannel);
-                 % 256 point fft
-                y_fft = fft(freqData ,256) ;
-                % plotting of spectra
-                p  = abs(y_fft.^2) ;
-                df = Fs/Npoint_fft ;
-                ff = 0:df:Fs/2-df ;
-                %save(filename,'p');
-
-                allFFT(n,:)= log(p(1:end/2)); 
-
-                plot (ff,log(p(1:end/2)),'b')
-                count = count+1;
-
-        end
-    
-        for n =1:length(allFFT(1,:))
-            szMaxFFT(n) = max(allFFT(:,n)); 
-
-        end
-
-        plot(ff, szMaxFFT,'r')
-        
-        allMaxFFTsz(i,:) = szMaxFFT;
-    
-end
-
-
-figure(3)
-hold on 
-for i=1: length(allMaxFFTSleep(:,1))
-    plot(ff,allMaxFFTsz(i,:),'r')
-      plot(ff,allMaxFFTSleep(i,:),'b')
 end
